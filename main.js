2eb15f5c46e463f986fb33a6fb4c772e657c6d73
Gabriella G. R <g.rozenaite@gmail.com>
2018-04-25 17:22:41 +0200

    Fixed js anim

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let btnPickup = document.getElementById('btn__choice--pickup');
 let btnStrip = document.getElementById('btn__choice--strip');
 // sounds
 let soundStrip = document.getElementById('sound_strip');
+let soundSlideIn = document.getElementById('sound_slidein');
 // frames
 let frameOne = document.getElementById('frame1');
 let frameTwo = document.getElementById('frame2');
@@ -16,6 +17,9 @@ let johnnyOne = document.getElementById('johnny1');
 btnPlay.addEventListener('click', function(){
   frameOne.style.display = 'none';
   frameTwo.style.display = 'initial';
+  johnnyOne.classList.add('anim-slide_in');
+  soundSlideIn.play();
+
 })
 // frame2
   btnStrip.addEventListener('click', function(){
