1061bda4d6faefac548aee05e3e8368c15c37dcc
Gabriella G. R <g.rozenaite@gmail.com>
2018-04-25 15:06:35 +0200

    Frames idea incorporated
    Frames Added
    Working button to play added

---
+++ main.js
@@ -0,0 +1,14 @@
+// OUR VARIABLES
+// buttons
+let btnPlay = document.getElementById('btn_play');
+
+// frames
+let frameOne = document.getElementById('frame1');
+let frameTwo = document.getElementById('frame2');
+
+// FRAMES
+// frame1
+btnPlay.addEventListener('click', function(){
+  frameOne.style.display = 'none';
+  frameTwo.style.display = 'initial';
+})
