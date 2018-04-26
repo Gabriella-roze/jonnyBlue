458a8bbe4bdaa73034a0fbd4701ae513fa77b82d
monikapuk <pukmonika98@gmail.com>
2018-04-26 15:50:07 +0200

    a lot stuff

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const btnStrip = document.getElementById('btn__choice--strip');
 // sounds
 const soundStrip = document.getElementById('sound_strip');
 const soundSlideIn = document.getElementById('sound_slidein');
+const soundHeyMama = document.getElementById('sound_heymama');
+const soundPickUp = document.getElementById('sound_pickup');
+const soundGoAway = document.getElementById('sound_goaway');
 // frames
 const frameOne = document.getElementById('frame1');
 const frameTwo = document.getElementById('frame2');
@@ -17,16 +20,45 @@ const johnnyTwo = document.getElementById('johnny2');
 
 // FRAMES
 // frame1
-btnPlay.addEventListener('click', function(){
+btnPlay.addEventListener('click', function() {
   frameOne.style.display = 'none';
   frameTwo.style.display = 'initial';
   johnnyOne.classList.add('anim-slide_in');
   soundSlideIn.play();
-
+  heymama();
 })
+
+function heymama() {
+  setTimeout(function() {
+    soundHeyMama.play();
+  }, 600);
+};
+
 // frame2
-  btnStrip.addEventListener('click', function(){
-    johnnyOne.classList.add('anim-strip');
+btnStrip.addEventListener('click', function() {
+  johnnyOne.classList.add('anim-strip');
+  soundStrip.play();
+  btnStrip.style.display = 'none';
+  strip2();
+  goaway();
+})
+
+function strip2() {
+  setTimeout(function() {
+    johnnyOne.style.display = 'none';
+    johnnyTwo.style.display = 'initial';
     soundStrip.play();
-    btnStrip.style.display='none';
+    johnnyTwo.classList.add('anim-strip');
+  }, 100);
+};
+
+function goaway() {
+  setTimeout(function() {
+    soundGoAway.play();
+  }, 1200);
+};
+
+btnPickup.addEventListener('click', function() {
+  soundPickUp.play();
+  btnPickup.style.display = 'none';
 })
