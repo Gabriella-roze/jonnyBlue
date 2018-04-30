8a8ae6b006f4588b7be80d23ee2c1dbcfed54c37
Gabriella G. R <g.rozenaite@gmail.com>
2018-04-30 15:20:44 +0200

    Revert "."
    This reverts commit e5aeb2b080874ad8904594a8a05a6619d79b6bf2.

--- main.js
+++ main.js
@@ -186,4 +186,5 @@ ladyBoy.addEventListener('click', function(){
 musician.addEventListener('click', function(){
   musician.style.cursor = 'pointer';
   soundString.play();
+  hand.classList.add('anim-hand');
 })
